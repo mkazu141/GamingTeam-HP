736c3f2f2b24afa59e6fd6a46724edf5d2a9c274
mkazu141 <104476684+mkazu141@users.noreply.github.com>
2022-06-15 19:04:03 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//window.addElementListener("load",function(){
+//  setTimeout(function(){
+//    document.getElementById("loading").classList.remove("active");
+//  },500)
+//})
+
+setTimeout(function(){
+  document.getElementById("loading").classList.remove("active");
+},3500)
+
+setTimeout(function(){
+  document.getElementById("after").classList.add("active");
+},3499)
+
+
+$('.slider').slick({
+  slidesToShow: 1,
+  autplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+  cssEase: 'linear',
+  asNavFor: '.thumb',
+});
+
+$('.thumb').slick({
+  slidesToShow: 4,
+  sliderToScroll: 1,
+  asNavFor: '.slider',
+  focusOnSelect: true,
+  dots: true,
+});
+
+
+
+
+
+const targetElement = document.querySelectorAll(".animationTarget");
+console.log("画面の高さ",window.innerHeight)
+document.addEventListener("scroll",function(){
+  for  (let i = 0; i < targetElement.length; i++){
+    const getElementDistance = targetElement[i].
+    getBoundingClientRect().top + targetElement[i].clientHeight*.6
+    if(window.innerHeight > getElementDistance){
+        targetElement[i].classList.add("show");
+    }
+  }
+});
